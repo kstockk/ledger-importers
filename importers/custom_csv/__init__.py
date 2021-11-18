8b9bfa12344b80afbfb27abea45f60c739ecc597
Khanh Nguyen <lykhee@outlook.com>
2021-11-18 13:19:07 +1100

    Added config.yaml accounts mapping and also single tag import function

--- importers/custom_csv/__init__.py
+++ importers/custom_csv/__init__.py
@@ -11,6 +11,7 @@ from dateutil.parser import parse
 import csv
 import os
 import re
+import yaml
 
 # Credit to https://gist.github.com/mterwill/7fdcc573dc1aa158648aacd4e33786e8#file-importers-chase-py
 
@@ -21,14 +22,21 @@ class CSVImporter(importer.ImporterProtocol):
     def extract(self, f):
         entries = []
 
+        with open('config.yaml', 'r') as stream:
+            try:
+                yaml_config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
         with open(f.name) as f:
             for index, row in enumerate(csv.DictReader(f)):
-                trans_acc = row['Account']
+                trans_account = row['Account']
                 trans_date = parse(row['Date']).date()
                 trans_payee = row['Payee']
-                trans_cat = row['Category']
+                trans_category = row['Category']
                 trans_desc = row['Memo']
                 trans_amt  = row['Amount']
+                trans_tag = row['Tags']
                 
                 meta = data.new_metadata(f.name, index)
 
@@ -38,17 +46,20 @@ class CSVImporter(importer.ImporterProtocol):
                     flag=flags.FLAG_OKAY,
                     payee=trans_payee,
                     narration=trans_desc,
-                    tags=set(),
+                    tags=set(tuple([trans_tag])),
                     links=set(),
                     postings=[],
                 )
 
+                account = yaml_config['account_map'].get(trans_account) or trans_account
+                category = yaml_config['account_map'].get(trans_category) or trans_category
+                
                 txn.postings.append(
-                    data.Posting(trans_acc, amount.Amount(D(trans_amt),
+                    data.Posting(account, amount.Amount(D(trans_amt),
                         'AUD'), None, None, None, None)
                 )
                 txn.postings.append(
-                    data.Posting(trans_cat, None, None, None, None, None)
+                    data.Posting(category, None, None, None, None, None)
                 )
 
                 entries.append(txn)
