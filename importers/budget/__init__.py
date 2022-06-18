c27ac6c45d825d45c8224c086afaf3f44a56fdaa
Khanh Nguyen <lykhee@outlook.com>
2022-06-18 21:12:54 +1000

    Added grouping based on various conditions. Very messy code...

--- importers/budget/__init__.py
+++ importers/budget/__init__.py
@@ -13,7 +13,8 @@ import csv
 import os
 import re
 import collections
-
+from itertools import groupby
+from operator import itemgetter
 # Credits to https://gist.github.com/mterwill/7fdcc573dc1aa158648aacd4e33786e8#file-importers-chase-py
 
 class CSVImporter(importer.ImporterProtocol):
@@ -25,54 +26,96 @@ class CSVImporter(importer.ImporterProtocol):
 
         account_map = CSVImporter.get_account_map()
 
-        with open(f.name) as f:
-            for index, row in enumerate(csv.DictReader(f)):
-                # Assign variables
-                account = row["Account"]
-                trans_date = parse(row["Date"]).date()
-                payee = row["Payee"]
-                desc = row["Notes"]
-                category = row["Category"]
-                amnt = row["Amount"]
+        with open(f.name, mode='r') as f:
+            rows = []
+            reader = csv.DictReader(f)
+            for row in reader:
+                rows.append(row)
 
-                # Search and replace account and category according to the account map
-                for ledger, budget in account_map.items():
-                    if account == budget and account != "":
-                        account = ledger
-                    if category == budget and category != "":
-                        category = ledger
+        # Blank categories are assumed to be transfers
+        transfers = []
+        for idx, row in enumerate(rows):
+            if not row['Category']:
+                row['Notes'] = row['Payee']
+                row['Payee'] = "Transfer"
+            
+            row["Abs"] = abs(D(row["Amount"]))
+                
+            # Search and replace account and category according to the account map
+            for ledger, budget in account_map.items():
+                if row["Account"] == budget and row["Account"]:
+                    row["Account"] = ledger
+                if row["Category"] == budget and row["Category"]:
+                    row["Category"] = ledger
 
+        # Group rows for postings
+        grouper = itemgetter("Date", "Account", "Payee", "Notes")
+        rows = sorted(rows, key = grouper)
+
+        for index, (key, value) in enumerate(groupby(rows, key = grouper)):
+            if key[2] != "Transfer":
                 meta = data.new_metadata(f.name, index)
 
                 txn = data.Transaction(
                     meta=meta,
-                    date=trans_date,
+                    date=parse(key[0]).date(),
                     flag=flags.FLAG_OKAY,
-                    payee=payee if category != "" else "Transfer",
-                    narration=desc if category != "" else payee,
+                    payee=key[2],
+                    narration=key[3],
                     tags=set(),
                     links=set(),
                     postings=[],
                 )
-                
-                txn.postings.append(
-                    data.Posting(account, amount.Amount(D(amnt),
+
+                total = 0
+                for t in value:
+                    txn.postings.append(
+                        data.Posting(t["Category"], amount.Amount(D(t["Amount"])*-1,
+                            "AUD"), None, None, None, None)
+                    )
+
+                    total += D(t["Amount"])
+
+                txn.postings.insert(0,
+                    data.Posting(key[1], amount.Amount(total,
                         "AUD"), None, None, None, None)
                 )
 
-                if category != "":
-                    txn.postings.append(
-                        data.Posting(category, amount.Amount(D(amnt)*-1,
+                entries.append(txn)
+
+        grouper = itemgetter("Date", "Payee", "Abs")
+        transfers = sorted(rows, key = grouper)
+
+        for index, (key, value) in enumerate(groupby(transfers, key = grouper)):
+            if key[1] == "Transfer":
+                meta = data.new_metadata(f.name, index)
+
+                txn = data.Transaction(
+                    meta=meta,
+                    date=parse(key[0]).date(),
+                    flag=flags.FLAG_OKAY,
+                    payee=key[1],
+                    narration="",
+                    tags=set(),
+                    links=set(),
+                    postings=[],
+                )
+
+                for t in value:
+                    position = 0 if D(t["Amount"]) < 0 else 1
+                    txn.postings.insert(position,
+                        data.Posting(t["Account"], amount.Amount(D(t["Amount"]),
                             "AUD"), None, None, None, None)
                     )
 
                 entries.append(txn)
 
+
         return entries
 
     @staticmethod
     def get_account_map():
-        with open("/bean/data/account_map.csv") as f:
+        with open("bean/data/account_map.csv") as f:
             reader = csv.reader(f)
             account_map = {rows[0]:rows[1] for rows in reader}
             return account_map
