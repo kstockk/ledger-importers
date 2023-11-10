c3cc54be6a5a2d38097656ecf09668ecd0258dfd
Khanh Nguyen <nguyenk3@outlook.com>
2023-11-10 13:46:40 +1100

    updated ioof to use mapping csv for journal mapping

--- importers/ioof/__init__.py
+++ importers/ioof/__init__.py
@@ -24,6 +24,34 @@ class IOOFImporter(importer.ImporterProtocol):
             header = f.readline().strip().split(',')
         return header == CSV_HEADER
 
+    def get_mappings(self):
+        # Get account mapping for Budget accounts --> Ledger accounts
+        # CSV should contain three columns "Budget Account, Ledger Account, Off-Budget"
+        # 1nd Column (Budget Acount) will be the key
+        try:
+            # found_csv = os.path.exists('ioof_transactions_mapping.csv')
+            # csv_path = BEAN_DATA_DIR + "/" if not found_csv else ""
+            with open('importers/ioof/ioof_transactions_mappings.csv', encoding='utf-8-sig') as f:
+                header = f.readline().strip()
+                if re.match(header, "trans_type,account_1,account_1_value,account_2,account_2_value,asset_name_2,asset_code_2"):
+                    reader = csv.reader(f)
+                    account_map = {
+                        rows[0]: {'account_1': rows[1], 'account_1_value': rows[2],
+                                  'account_2': rows[3], 'account_2_value': rows[4],
+                                  'asset_name_2': rows[5], 'asset_code_2': rows[6]} for rows in reader
+                        }
+            return account_map
+        except:
+            return False
+
+    def get_map(self, trans_type, key):
+        try:
+            mappings = self.get_mappings()
+            account = mappings[trans_type][key]
+            return account
+        except KeyError as e:
+            return "no account mappings specified for {}".format(str(e))
+
     def extract(self, file_):
         # Create entries
         # Create transaction entries
@@ -51,65 +79,38 @@ class IOOFImporter(importer.ImporterProtocol):
                     postings=[],
                 )
 
-                if trans_type == "Deposit":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Income:Super:Concessional-Contributions:IOOF", amount.Amount(D(amnt)*-1,
-                            'AUD'), None, None, None, None)
-                    )
+                account_1 = self.get_map(trans_type, 'account_1')
+                account_2 = self.get_map(trans_type, 'account_2')
 
-                if trans_type == "Tax":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Expenses:Super:Tax:IOOF-Employer-Super", amount.Amount(D(amnt)*-1,
-                            'AUD'), None, None, None, None)
-                    )
+                if not "no account" in account_1 or not "no account" in account_2:
+                    amount_1 = D(amnt) * int(self.get_map(trans_type, 'account_1_value'))
+                    amount_2 = D(amnt) * int(self.get_map(trans_type, 'account_2_value'))
+                else:
+                    amount_1 = D(amnt)
+                    amount_2 = D(amnt) * -1
 
-                if trans_type == "Fees & costs":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Expenses:Super:Fees:IOOF-Employer-Super", amount.Amount(D(amnt)*-1,
-                            'AUD'), None, None, None, None)
-                    )
+                asset_name = self.get_map(trans_type, 'asset_name_2')
+                asset_code = self.get_map(trans_type, 'asset_code_2')
 
-                if trans_type == "Insurance":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Expenses:Super:Insurance:IOOF-Employer-Super", amount.Amount(D(amnt)*-1,
-                            'AUD'), None, None, None, None)
-                    )
+                cur_2 = 'AUD'
+                cost_2 = None
 
-                if trans_type == "Buys":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:IOF0097AU", amount.Amount(D(units),
-                            'IOF0097AU'), Cost(D(unit_price), 'AUD', None, None), None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
+                if asset_name == desc:
+                    amount_2 = D(units)
+                    cur_2 = asset_code
+                    cost_2 = Cost(D(unit_price), 'AUD', None, None)
 
-                if trans_type == "Income":
-                    txn.postings.insert(0,
-                        data.Posting("Assets:Super:IOOF-Employer-Super:Cash", amount.Amount(D(amnt),
-                            'AUD'), None, None, None, None)
-                    )
-                    txn.postings.insert(1,
-                        data.Posting("Income:Super:Income", amount.Amount(D(amnt)*-1,
-                            'AUD'), None, None, None, None)
-                    )
+                index_1 = 0 if amount_1 >= 0 else 1
+                index_2 = 1 if index_1 == 0 else 0
+
+                txn.postings.insert(index_1,
+                    data.Posting(account_1, amount.Amount(D(amount_1),
+                        'AUD'), None, None, None, None)
+                )
+                txn.postings.insert(index_2,
+                    data.Posting(account_2, amount.Amount(D(amount_2),
+                        cur_2), cost_2, None, None, None)
+                )
 
                 entries.append(txn)
 
